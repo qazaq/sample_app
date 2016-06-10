6999b0571f8d4a5ffba7be8b743ff8dc02411ad4
strivemag <talga.tawit@gmail.com>
2016-06-10 12:50:25 +0600

    Chapter 10

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ require File.expand_path("../../config/environment", __FILE__)
 
 Rails.application.routes.draw do
   
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
